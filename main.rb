c06edd9968a22a363f6714357076f44423bcfc22
Rob Dickinson <robfromboulder@gmail.com>
2017-09-24 22:29:49 -0600

    Use logger when present

--- main.rb
+++ main.rb
@@ -1,14 +1,15 @@
 require 'sinatra'
+logger = nil
 
 get '/' do
   response_body = 'Hello Sinatra!'
-  logger.log request, nil, response, response_body
+  logger.log request, nil, response, response_body unless logger.nil?
   response_body
 end
 
 post '/' do
   request_body = request.body.read
   response_body = 'Posted to Sinatra: ' + request_body
-  logger.log request, request_body, response, response_body
+  logger.log request, request_body, response, response_body unless logger.nil?
   response_body
 end
