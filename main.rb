f34457feab585e33e025d6de103b384c4a320b7c
RobDickinson <RobDickinson@users.noreply.github.com>
2017-11-08 20:48:11 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,13 +3,13 @@ logger = nil
 
 get '/' do
   response_body = 'Hello Sinatra!'
-  logger.log request, nil, response, response_body unless logger.nil?
+  logger.log request, response, response_body unless logger.nil?
   response_body
 end
 
 post '/' do
   request_body = request.body.read
   response_body = 'Posted to Sinatra: ' + request_body
-  logger.log request, request_body, response, response_body unless logger.nil?
+  logger.log request, response, response_body, request_body unless logger.nil?
   response_body
 end
