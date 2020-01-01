3b078a26ffa96d2e2fd5178a7d33aa7d40b2c61b
Rob Dickinson <robfromboulder@gmail.com>
2020-01-01 12:58:52 -0700

    Update for v2 API

--- main.rb
+++ main.rb
@@ -3,13 +3,13 @@ logger = nil
 
 get '/' do
   response_body = 'Hello Sinatra!'
-  logger.log request, response, response_body unless logger.nil?
+  HttpMessage.send(logger, request, response, response_body) unless logger.nil?
   response_body
 end
 
 post '/' do
   request_body = request.body.read
   response_body = 'Posted to Sinatra: ' + request_body
-  logger.log request, response, response_body, request_body unless logger.nil?
+  HttpMessage.send(logger, request, response, response_body, request_body) unless logger.nil?
   response_body
 end
